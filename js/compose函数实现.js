df356a05ffc2f23358fcf793922df75d83708635
yangfeng <crazyyoung1020@163.com>
2022-01-11 01:41:26 +0800

    compose函数和惰性函数注释

--- js/compose函数实现.js
+++ js/compose函数实现.js
@@ -24,15 +24,21 @@ var flowRight = function(funcs) {
 // 递归
 
 var compose = function(...args) {
+    // 传入的函数列表的长度
     var len = args.length
+    // 最后一个函数的下表
     var count = len - 1
+    // 处理的结果
     var result
     return function f1(...args1) {
+        // 用函数参数数组的末尾一个函数，计算出结果
         result = args[count].apply(this, args1)
+        // 如果末尾函数，已经是仅剩的函数了，则直接返回计算结果
         if (count <= 0) {
             count = len - 1
             return result
         } else {
+            // 如果末尾函数不是仅剩的函数，则拿着计算结果，继续去递归执行，知道最后一个函数为止
             count--
             return f1.call(null, result)
         }
--- js/惰性函数.js
+++ js/惰性函数.js
@@ -1,3 +1,7 @@
+// 第一次申明addEvent变量后，会给它赋值一个function
+// 后序再调用addEvent的时候，就直接走这个function了，不会再走判断了
+
+
 var addEvent = (function () {
     if (document.addEventListener) {
         return function (type, element, fun) {
